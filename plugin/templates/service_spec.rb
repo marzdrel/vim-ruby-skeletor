0a22c6c49f1ac8933475fb768f36384022374efe
Marz Drel <marzdrel@users.noreply.github.com>
2019-05-04 00:50:06 +0200

    Add basic spec template

---
+++ plugin/templates/service_spec.rb
@@ -0,0 +1,12 @@
+require "rails_helper"
+
+RSpec.describe [:CLASSNAME:] do
+  let(:service) { described_class.new(params) }
+  let(:params) { }
+
+  describe "#call" do
+    it "description" do
+      pending "#{__FILE__}"
+    end
+  end
+end
