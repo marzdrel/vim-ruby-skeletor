fb36a32fbd781998923e68a0b64c72ebe551353b
Marz Drel <marzdrel@users.noreply.github.com>
2019-05-10 20:31:54 +0200

    Add final Parser logic to Performer

---
+++ ruby/app/parser/performer.rb
@@ -0,0 +1,26 @@
+require_relative "../parser.rb"
+require_relative "../vim_aliases.rb"
+
+class Parser::Performer
+  def self.call(*args)
+    new(*args).call
+  end
+
+  def initialize(pathname)
+    self.pathname = pathname
+  end
+
+  def call
+    if pathname.length.zero?
+      VimC %Q{echo "Can't extract any Class name from current line."}
+    else
+      FileUtils.mkdir_p File.dirname(pathname)
+      VimC %Q{echo "Opening: #{pathname}"}
+      VimC %Q{silent exec "tabe " . "#{pathname}" | execute ":redraw!"}
+    end
+  end
+
+  private
+
+  attr_accessor :pathname
+end
---
+++ ruby/spec/parser/performer_spec.rb
@@ -0,0 +1,47 @@
+require "spec_helper"
+require_relative "../../app/parser/performer.rb"
+require_relative "../support/squish.rb"
+require_relative "../support/vim.rb"
+
+RSpec.describe Parser::Performer do
+  using Squish
+
+  let(:service) { described_class.new(object) }
+  let(:object) { "app/services/file.rb" }
+
+  before do
+    allow(Vim).to receive_messages(
+      command: :command,
+      evaluate: :evaluate,
+    )
+
+    allow(FileUtils).to receive(:mkdir_p)
+  end
+
+  describe "#call" do
+    before { service.call }
+
+    context "with valid path" do
+      it "creates directory" do
+        expect(FileUtils)
+          .to have_received(:mkdir_p)
+          .with("app/services")
+      end
+
+      it "opens the file in vim" do
+        expect(Vim)
+          .to have_received(:command)
+          .with(<<-TXT.squish)
+            silent exec "tabe " . "app/services/file.rb" |
+            execute ":redraw!"
+          TXT
+      end
+
+      it "shows the message" do
+        expect(Vim)
+          .to have_received(:command)
+          .with %Q{echo "Opening: app/services/file.rb"}
+      end
+    end
+  end
+end
