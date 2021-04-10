bae351051e787aec327dc4807c0972792fa6d516
Marz Drel <marzdrel@users.noreply.github.com>
2021-04-10 16:21:14 +0200

    Change templates for service objects

--- plugin/templates/service_spec.rb
+++ plugin/templates/service_spec.rb
@@ -1,10 +1,16 @@
 require "rails_helper"
 
 RSpec.describe [:CLASSNAME:] do
-  let(:service) { described_class.new(params) }
-  let(:params) { }
+  describe ".call" do
+    subject { described_class }
+
+    it { should forward_to_instance(:call).with_1_args }
+  end
 
   describe "#call" do
+    let(:service) { described_class.new(params) }
+    let(:params) { {} }
+
     it "description" do
       pending "#{__FILE__}"
     end
